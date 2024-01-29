759bcbdfb9124de0c180e9f888cd0394e0125ffa
Cyprien Kelma <cyprien.kelma@gmail.com>
2024-01-29 10:02:12 +0100

    update main code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
+app.use("/api/users", require("./routes/usersRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
