59222f9a6c2c7b82c9525bc671dbc61699a91fa3
Cyprien Kelma <cyprien.kelma@gmail.com>
2024-01-29 15:57:09 +0100

    tailwind on

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import './css/App.css';
+import './css/index.css';
+import './css/styles.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        <p>
+        <p className='text-green-400'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link font-bold py-2 px-4 rounded"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import './css/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
