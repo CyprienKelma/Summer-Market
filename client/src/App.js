1d4b1b8ba1b8196956799b375336dc723ac03b39
Enzoooooooo <enzo.leroux@student.junia.com>
2024-01-29 16:03:31 +0100

    Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
+import Login from './components/login';
 import './css/App.css';
 import './css/index.css';
 import './css/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p className='text-green-400'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link font-bold py-2 px-4 rounded"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Login/>
+  )
+};
 
 export default App;
---
+++ client/src/components/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Bienvenue sur notre page d'accueil</h1>
+      // Ajoutez ici plus de contenu pour votre page d'accueil
+    </div>
+  );
+}
+
+export default Login;
