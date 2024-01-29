e7e25085c1a8e0199bdb793e09effe6da746d16b
Cyprien Kelma <cyprien.kelma@gmail.com>
2024-01-29 14:00:08 +0100

    update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.use("/api/users", require("./routes/usersRoutes"));
+app.use("/api/users", require("./server/routes/usersRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
