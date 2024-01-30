337701ce413bf17f2ccd413f0c45369e64a41fdb
Cyprien Kelma <cyprien.kelma@gmail.com>
2024-01-30 09:28:38 +0100

    change controller

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUserById } = require('../controllers/usersController');
+const { getUsers, getUserById } = require('../controller/usersController');
 
 // Path: server/routes/usersRoutes.js
 
