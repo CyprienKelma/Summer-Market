2bee2cca6243566616654d26e0d225d8780ffc34
Enzoooooooo <enzo.leroux@student.junia.com>
2024-01-30 11:13:51 +0100

    Products

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { connectDB } = require("./config/db"); // Importer la fonction de connexion
+const productRoutes = require('./routes/productRoutes');; // Assurez-vous que le chemin est correct
+
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use("/api/users", require("./routes/usersRoutes"));
+app.use('/api/products', require("./routes/productRoutes")); // Utiliser '/api/products' comme base pour les routes de produits
 
 // Établir la connexion à la base de données au démarrage du serveur
 connectDB(process.env.MONGO_URI).then(() => {
