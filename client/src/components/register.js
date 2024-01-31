b138d3a6ea0215543f7146f93cbdc434b7cf4861
Enzoooooooo <enzo.leroux@student.junia.com>
2024-01-31 16:48:38 +0100

    MAJ

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -73,7 +73,7 @@ function Register() {
 
         try {
             // Utiliser Axios pour envoyer les données au serveur
-            const response = await axios.post('https://10.224.1.139:5001/', {
+            const response = await axios.post('https://10.224.1.139:300/', {
                 username,
                 email,
                 password,
